eac176061c1341071b3362d8153f85a3aabfbd2d
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2022-09-12 20:41:10 +0700

    Upload tugas teori

---
+++ src/Tugas_PBO_Teori/HeroMobileLegend.java
@@ -0,0 +1,70 @@
+/**
+ * Author   : Muhamad Ardi Nur Insan
+ * NIM      : 211511022
+ * Kelas    : D3 Teknik Informatika
+ * Filename ; HeroMobileLegend.java
+ */
+
+package Tugas_PBO_Teori;
+
+import java.util.Random;
+
+public class HeroMobileLegend {
+	
+	private String nameHero;
+	private String roleHero;
+	private int heroPower = 0;
+	
+	Random randomObject = new Random();
+	
+	// constructor class
+	public HeroMobileLegend(String nameHero, String roleHero, int heroPower) {
+		this.setNameHero(nameHero);
+		this.setRoleHero(roleHero);
+		this.setHeroPower(heroPower);
+	}
+
+	public void setNameHero(String nameHero) {
+		this.nameHero = nameHero;
+	}
+
+	public void setRoleHero(String roleHero) {
+		this.roleHero = roleHero;
+	}
+
+	// mencoba menggunakan encapsulasi protected pada method 
+	protected int setHeroPower(int heroPower) {
+		return this.heroPower = heroPower;
+	}
+    
+	// hero main programnya
+	public void getHeroData() {
+		System.out.println("Nama Hero  : " + nameHero);
+		System.out.println("Role Hero  : " + roleHero);
+		System.out.println("Hero Power : " + heroPower);
+	}
+	
+	private String setMatchClassic(String namaHero, String roleHero, int heroPower) {
+		// random integer dan lalu di casting ke format integer
+		int heroComputerRandom =  randomObject.nextInt(1000);
+		String kata;
+		
+		if(heroPower >= heroComputerRandom) {
+			kata = "Yeayy kamu Memenangkan pertandingan !";
+		}else if(heroPower <= heroComputerRandom) {
+			kata = "Yahhh kamu kalah !";
+		} else {
+			kata = "tidak diketahui";
+		}
+		
+		return kata;
+	}
+	
+	public void runMatchClassic() {
+		String kataTemp;
+		kataTemp = this.setMatchClassic(this.nameHero, this.roleHero, this.heroPower);
+		System.out.print("Status hasil pertandingan : ");
+		System.out.println(kataTemp);
+	}
+	
+}
---
+++ src/Tugas_PBO_Teori/MyHero.java
@@ -0,0 +1,26 @@
+/**
+ * Author   : Muhamad Ardi Nur Insan
+ * NIM      : 211511022
+ * Kelas    : D3 Teknik Informatika
+ * Filename ; MyHero.java
+ */
+
+package Tugas_PBO_Teori;
+
+public class MyHero {
+	public static void main(String[] args) {
+		HeroMobileLegend myHero1 = new HeroMobileLegend("Alucard", "Fighter", 333);
+		System.out.println("\nHero 1");
+		myHero1.getHeroData();
+		System.out.println("Status Hero : Sedang Betanding Classic dengan Computer");
+		myHero1.runMatchClassic();
+		
+		System.out.println("\n------------------------------------------------");
+		
+		HeroMobileLegend myHero2 = new HeroMobileLegend("Moskov", "Marksman", 500);
+		System.out.println("\nHero 2");
+		myHero2.getHeroData();
+		System.out.println("Status : Tidak dalam Match");
+	}
+
+}
